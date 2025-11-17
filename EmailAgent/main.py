d25021ac3312f6fffdf83d26f39d7417de6a1f70
jaswanth.sudha <jaswanth.sudha@peopletech.com>
2025-11-17 18:16:56 +0530

    Refactor main.py to streamline model initialization and remove unused code; update .gitignore to include .env

--- EmailAgent/main.py
+++ EmailAgent/main.py
@@ -1,24 +1,13 @@
-from langchain.agents import create_agent
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
-from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_huggingface import HuggingFaceEndpoint
 
-model = HuggingFaceEndpoint(
-    repo_id="microsoft/Phi-3-mini-4k-instruct",
+
+llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-Nemo-Instruct-2407",  # Any free text-generating model
     task="text-generation",
-    max_new_tokens=512,
-    do_sample=False,
-    repetition_penalty=1.03,
 )
 
-chat = ChatHuggingFace(llm=model, verbose=True)
-messages = [
-    ("system", "You are a helpful translator. Translate the user sentence to French."),
-    ("human", "I love programming."),
-]
-try:
-    chat.invoke(messages)
-except Exception as e:
-    print(f"Error during chat invocation: {e}")
+response = llm.generate(["Write a poem about AI assisting humans:"])
+print(response)
